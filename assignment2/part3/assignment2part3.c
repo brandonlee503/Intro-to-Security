7d143e1933be584e0128b5ad9b762e8ab7a5a065
Brandon Lee <brandonlee503@gmail.com>
2016-11-20 19:49:24 -0800

    Add strong collision resistance

--- assignment2/part3/assignment2part3.c
+++ assignment2/part3/assignment2part3.c
@@ -41,6 +41,8 @@ void weakCollisionResistance(int argc, char *argv[]) {
         exit(1);
     }
 
+    printf("WEAK COLLISION RESISTANCE\n");
+
     // Generate random string
     unsigned char randomString[25];
     rand_str(randomString, 25);
@@ -73,11 +75,70 @@ void weakCollisionResistance(int argc, char *argv[]) {
     }
 }
 
+void strongCollisionResistance(int argc, char *argv[]) {
+    EVP_MD_CTX *mdctx;
+    const EVP_MD *md;
+    unsigned char md_value[EVP_MAX_MD_SIZE];
+    unsigned char first_md_value[EVP_MAX_MD_SIZE];
+    int md_len, i;
+
+    // Initialize random number generator
+    time_t t;
+    srand((unsigned) time(&t));
+
+    // Boilerplate EVP setup
+    OpenSSL_add_all_algorithms();
+    if (!argv[1]) {
+        printf("Usage: mdtest digestname\n");
+        exit(1);
+    }
+
+    md = EVP_get_digestbyname(argv[1]);
+
+    if (!md) {
+        printf("Unknown message digest %s\n", argv[1]);
+        exit(1);
+    }
+
+    printf("STRONG COLLISION RESISTANCE\n");
+
+    // Generate random string
+    unsigned char randomString[25];
+
+    // Iterate until collision is found
+    int counter = 0;
+    while (1) {
+        // Iteratively generate random strings and digests
+        rand_str(randomString, 25);
+        mdctx = EVP_MD_CTX_create();
+        EVP_DigestInit_ex(mdctx, md, NULL);
+        EVP_DigestUpdate(mdctx, randomString, strlen(randomString));
+        EVP_DigestFinal_ex(mdctx, first_md_value, &md_len);
+        EVP_MD_CTX_destroy(mdctx);
+
+        rand_str(randomString, 25);
+        mdctx = EVP_MD_CTX_create();
+        EVP_DigestInit_ex(mdctx, md, NULL);
+        EVP_DigestUpdate(mdctx, randomString, strlen(randomString));
+        EVP_DigestFinal_ex(mdctx, md_value, &md_len);
+        EVP_MD_CTX_destroy(mdctx);
+
+        // Compare the first 3 bytes (24 bits)
+        if (memcmp(md_value, first_md_value, 3) == 0) {
+            printf("Collision found! Number of attempts: %i\n", counter);
+            break;
+        } else {
+            counter++;
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     int i;
     for (i = 0; i < 100; i++) {
         weakCollisionResistance(argc, argv);
+        strongCollisionResistance(argc, argv);
     }
-    
+
     exit(0);
 }
